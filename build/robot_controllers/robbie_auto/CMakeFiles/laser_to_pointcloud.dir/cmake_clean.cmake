ac6cccb8506aeb8886ffa476eab3524d8e6b8656
MoyinOtubela <otubelam@tcd.ie>
2017-03-16 18:50:57 +0000

    lhm controller improved

---
+++ build/robot_controllers/robbie_auto/CMakeFiles/laser_to_pointcloud.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laser_to_pointcloud.dir/src/laser_to_pointcloud.cpp.o"
+  "/home/moyin/dev/autonomous_controllers/devel/lib/robbie_auto/laser_to_pointcloud.pdb"
+  "/home/moyin/dev/autonomous_controllers/devel/lib/robbie_auto/laser_to_pointcloud"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laser_to_pointcloud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
